66fbcee1edc8ca88136e795c6d2e3a5cdae03015
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2024-08-27 17:14:38 +0300

    Dashboard Before Refactor Styled

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,6 @@ import { useUser } from '../features/authentication/useUser';
 import UploadTodoModal from '../features/Dashboard/UploadTodoModal';
 import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog';
 import supabase from '../services/supabase';
-
 import { toast } from 'react-hot-toast';
 
 function Dashboard() {
@@ -15,6 +14,7 @@ function Dashboard() {
   const queryClient = useQueryClient();
 
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     async function cleanUpOverdueTodos() {
@@ -94,7 +94,13 @@ function Dashboard() {
   }
 
   // Prepare data
-  const topUsers = users.slice(0, 100);
+  const filteredUsers = useMemo(() => {
+    return users
+      .filter((user) =>
+        user.full_name.toLowerCase().includes(searchTerm.toLowerCase()),
+      )
+      .slice(0, 100);
+  }, [users, searchTerm]);
 
   const top5Users = useMemo(() => {
     return users
@@ -122,67 +128,141 @@ function Dashboard() {
   }
 
   return (
-    <div className="p-6 bg-gray-100 min-h-screen">
+    <div className="p-6 bg-teal-100 2xl:min-h-screen">
       {/* Confirmation Dialog */}
       <ConfirmDialog acceptClassName="ml-3" />
 
       {/* Welcome Message */}
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-        <h1 className="text-3xl font-bold">
-          Welcome, {user?.full_name || 'User'}
-        </h1>
+      <div className=" p-6 mb-6">
+        <h1 className="text-3xl font-bold">Welcome, {user?.full_name}</h1>
       </div>
 
-      {/* Container with First 100 Users */}
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6 max-h-60 overflow-y-scroll">
-        <h2 className="text-xl font-semibold mb-4">First 100 Users</h2>
-        <ul>
-          {topUsers.map((user) => (
-            <li key={user.id} className="py-2 border-b border-gray-300">
-              {user.full_name} - {user.email}
-            </li>
-          ))}
-        </ul>
-      </div>
+      {/* Dashboard Content */}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-6">
+        {/* First 100 Users */}
+        <div
+          className="bg-white shadow-md rounded-lg p-6 overflow-y-auto row-span-2 max-h-[605px]"
+          // style={{ width: '300px' }}
+        >
+          <div className="flex justify-between items-center mb-4">
+            <h2 className="text-2xl font-semibold">First 100 Users</h2>
+            <input
+              type="text"
+              placeholder="Search by name"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="border border-gray-300 rounded px-2 py-1"
+            />
+          </div>
+          <ul style={{ overflowY: 'auto' }}>
+            {filteredUsers.slice(0, 15).map((user) => (
+              <>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+                <li key={user.id} className="py-2 border-b border-gray-300">
+                  {user.full_name} - {user.email}
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
 
-      {/* To-Do List */}
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-        <div className="flex justify-between items-center">
-          <h2 className="text-xl font-semibold mb-4">To-Do List</h2>
-          {(user.role === 'admin' || user.role === 'super_admin') && (
-            <button
-              onClick={() => setIsModalOpen(true)}
-              className="bg-blue-500 text-white px-4 py-2 rounded"
-              disabled={createMutation.isLoading}
-            >
-              {createMutation.isLoading ? 'Creating...' : 'Create To-Do'}
-            </button>
+        {/* To-Do List */}
+        <div className="bg-white shadow-md rounded-lg p-6 max-h-[300px] overflow-y-auto row-start-1 col-start-2 col-span-2">
+          <div className="flex justify-between text-center">
+            <h2 className="text-3xl font-semibold mb-4">To-Do List</h2>
+            {(user.role === 'admin' || user.role === 'super_admin') && (
+              <button
+                onClick={() => setIsModalOpen(true)}
+                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-all duration-300"
+                disabled={createMutation.isLoading}
+              >
+                {createMutation.isLoading ? 'Creating...' : 'Create To-Do'}
+              </button>
+            )}
+          </div>
+
+          {/* Check if there are any To-Dos */}
+          {toDos.length === 0 ? (
+            <div className="text-center text-gray-500">
+              No assignments are due. Enjoy your day!
+            </div>
+          ) : (
+            <ul>
+              {toDos.map((todo) => (
+                <li
+                  key={todo.id}
+                  className="py-2 border-b border-gray-300 flex justify-between items-center"
+                >
+                  <div>
+                    <div className="font-semibold text-lg">{todo.title}</div>
+                    <div className="text-gray-600 text-lg">
+                      Deadline: {new Date(todo.deadline).toLocaleDateString()}
+                    </div>
+                  </div>
+                  {(user.role === 'admin' || user.role === 'super_admin') && (
+                    <button
+                      onClick={() => handleDeleteTodo(todo.id)}
+                      className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition-all duration-300"
+                    >
+                      Delete
+                    </button>
+                  )}
+                </li>
+              ))}
+            </ul>
           )}
         </div>
 
-        <ul>
-          {toDos.map((todo) => (
-            <li
-              key={todo.id}
-              className="py-2 border-b border-gray-300 flex justify-between items-center"
-            >
-              <div>
-                <div className="font-semibold">{todo.title}</div>
-                <div className="text-gray-600">
-                  Deadline: {new Date(todo.deadline).toLocaleDateString()}
+        {/* Top 5 Users Who Uploaded the Most Notes */}
+        <div className="bg-white shadow-md rounded-lg p-6 max-h-[400px] flex flex-col">
+          <h2 className="text-2xl font-semibold mb-5">
+            Top 5 Users by Uploaded Notes
+          </h2>
+          <ul className="overflow-y-auto flex-grow">
+            {top5Users.map((user) => (
+              <li
+                key={user.id}
+                className="py-2 border-b border-gray-300 text-xl"
+              >
+                {user.full_name} - {user.uploadedNotesCount}
+              </li>
+            ))}
+          </ul>
+          <button className="mt-4 bg-blue-500 text-white p-4 rounded hover:bg-blue-600 transition-all duration-300 text-xl">
+            Upload your Notes!
+          </button>
+        </div>
+
+        {/* Top 5 Most Rated Notes */}
+        <div className="bg-white shadow-md rounded-lg p-6 max-h-[400px] overflow-y-auto">
+          <h2 className="text-2xl font-semibold mb-4">
+            Top 5 Most Rated Notes
+          </h2>
+          <ul>
+            {top5Notes.map((note) => (
+              <li key={note.id} className="py-2 border-b border-gray-300">
+                <div className="font-semibold text-xl">{note.title}</div>
+                <div className="text-gray-600 text-lg">
+                  Average Rating: {note.average_rating} - {note.users.full_name}
                 </div>
-              </div>
-              {(user.role === 'admin' || user.role === 'super_admin') && (
-                <button
-                  onClick={() => handleDeleteTodo(todo.id)}
-                  className="bg-red-500 text-white px-4 py-2 rounded"
-                >
-                  Delete
-                </button>
-              )}
-            </li>
-          ))}
-        </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
 
       <UploadTodoModal
@@ -190,35 +270,6 @@ function Dashboard() {
         onClose={() => setIsModalOpen(false)}
         onSubmit={handleCreateToDo}
       />
-
-      {/* Top 5 Users Who Uploaded the Most Notes */}
-      <div className="bg-white shadow-md rounded-lg p-6 mb-6">
-        <h2 className="text-xl font-semibold mb-4">
-          Top 5 Users by Uploaded Notes
-        </h2>
-        <ul>
-          {top5Users.map((user) => (
-            <li key={user.id} className="py-2 border-b border-gray-300">
-              {user.full_name} - {user.uploadedNotesCount}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      {/* Top 5 Most Rated Notes */}
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <h2 className="text-xl font-semibold mb-4">Top 5 Most Rated Notes</h2>
-        <ul>
-          {top5Notes.map((note) => (
-            <li key={note.id} className="py-2 border-b border-gray-300">
-              <div className="font-semibold">{note.title}</div>
-              <div className="text-gray-600">
-                Average Rating: {note.average_rating} - {note.users.full_name}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
     </div>
   );
 }
